fc5d7ce621e60cf835c933ef0b1cae34de96b989
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-06 16:27:54 -0800

    declared and printed the types of variables needed for Ch 1-4 / Class 1's exercises

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,8 +1,20 @@
 // Declare and assign the variables below
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
+//https://education.launchcode.org/intro-to-web-dev-curriculum/data-and-variables/exercises/index.html#the-data
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof (shuttleName));
+console.log(typeof (shuttleSpeedMph));
+console.log(typeof (distanceToMarsKm));
+console.log(typeof (distanceToMoonKm));
+console.log(typeof (milesPerKm));
 
 // Calculate a space mission below
+
 
 // Print the results of the space mission calculations below
 
