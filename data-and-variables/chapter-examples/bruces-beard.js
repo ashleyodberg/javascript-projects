3ae02e1da49edf2ca3a596c5b4c2933e2b2f8175
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-08 00:59:40 -0800

    reviewing data and variables chapter examples and debugging errors practice

--- data-and-variables/chapter-examples/bruces-beard.js
+++ data-and-variables/chapter-examples/bruces-beard.js
@@ -1,1 +1,2 @@
-console.log('Bruce's beard');
+console.log("Bruce's beard");
+//changed the quotes before and after the text to double quotes due to the apostrophe being interpreted as a single quote, throwing a module not found error bc ''Bruce' ' is not present in the file as a variable or object
