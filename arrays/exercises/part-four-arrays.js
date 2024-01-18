b4bf7b1d18df79d887767b88c4637df101a4c04d
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-17 22:41:20 -0800

    was able to wrap up the questions on part 4 of array exercises

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -10,6 +10,16 @@ console.log(concatenatedArray);
 console.log(holdCabinet1); //helps confirm the original array is unchanged by concat
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+let slicedArray1 = holdCabinet1.slice(0, 2);
+let slicedArray2 = holdCabinet2.slice(0, 2);
+console.log(slicedArray1);
+console.log(slicedArray2);
+console.log(holdCabinet1); //slice does not change the original array here
+console.log(holdCabinet2);//slice does not alter this original array either
 
 
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse();
+holdCabinet2.sort();
+console.log(holdCabinet1); //original array has been altered by the reverse method
+console.log(holdCabinet2); //original array has been altered by the sort method
