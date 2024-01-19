d440172ebfda3a689758feed2c123754b30f377d
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-19 15:21:53 -0800

    worked through the first section of examples in Ch 9 going over loops and strings and a bit of the loops with arrays practice.

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,12 @@
 // create an array variable containing the names
 
 // write a for loop that prints each name on a different line
+
+
+//Flow of Execution of the for loop practice (sec 2 of reading)
+for (let i = 0; i < 51; i++) { //When the program reaches the for loop, the initial expression, let i = 0, is executed, declaring the variable i and initializing it to the value 0.
+    //the loop condition is i < 51, and is evaluted, returning true because 0 is less than 51.
+    //Since the condition is true, the loop body executes, printing 0. Then the update expression, i++ is executed, setting i = 1, this completes the first full iteration of the loop.
+    //Steps 2-4 are repeated, using the new value of i, continuing until the loop condition evaluates to false in step 2, ending the loop. 
+    console.log(i);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,20 @@
 // Create a string variable containing your name.
-
+let myName = 'Ashley Nicholas';
+for (let i = 0; i < myName.length; i++) {
+    console.log(myName[i]);
+}
 
 // Write a for loop that prints each character in your name on a different line.
+//see above code
+
+
+
+
+
+//Iterating Over Strings section practice
+/*let name = "LaunchCode";
+
+for (let i = 0; i < name.length; i++) {
+    console.log(name[i]);
+} //since name.length is 10, the loop executes once each for the value of i from 0 to 9*/
+//The loop body (console.log(name[i]); will print name [i] each time
