2f5663ef4a5a87b80846f3e510c886d4e8aaabf5
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-19 16:12:07 -0800

    Worked through the 'breaking down the for statement' section of the reading and will return later.

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,20 @@
 // Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
 
-for (let i = 0; i < 51; i++) {
+/* for (let i = 0; i < 51; i++) {
    console.log(i);
- }
+}*/
+ 
+for (let i = 3; i < 10; i++) {
+  console.log(i);
+}
+
+let name = "LaunchCode";
+
+for (let i = 6; i < name.length; i++) {
+  console.log(name[i]);
+}
+
+for (let i = 0; i < 51; i = i + 2) {
+  console.log(i);
+}
+
