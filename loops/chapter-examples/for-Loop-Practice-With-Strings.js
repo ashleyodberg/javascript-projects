b25add9914a58241e80413be9aa34a3d4a0f8452
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-20 23:52:04 -0800

    Worked through remaining chapter examples and am wrapping up Ch 9 on Loops, more practice needed to cement concepts.

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -7,7 +7,16 @@ for (let i = 0; i < myName.length; i++) {
 // Write a for loop that prints each character in your name on a different line.
 //see above code
 
+//Breaking Down the for Statement
+for (let i = 0; i < 51; i = i + 2) {
+    console.log(i);
+}
 
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
 
 
 
@@ -18,3 +27,13 @@ for (let i = 0; i < name.length; i++) {
     console.log(name[i]);
 } //since name.length is 10, the loop executes once each for the value of i from 0 to 9*/
 //The loop body (console.log(name[i]); will print name [i] each time
+
+//The accumulator pattern practice
+let n = 6;
+let total = 0;
+
+for (let i = 1; i <= n; i++) {
+    total += i;
+}
+
+console.log(total);
