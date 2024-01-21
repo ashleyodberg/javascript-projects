2de720c57948b49ed64b3eba8cdcc86ed440f07d
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-21 00:31:32 -0800

    worked through another section, been working on my assignment as well so it's going a bit slower

--- loops/chapter-examples/Reversing-a-String.js
+++ loops/chapter-examples/Reversing-a-String.js
@@ -1,5 +1,7 @@
 let str = "blue";
-let reversed = "";
+let reversed = ""; //accumulator variable
+//approach will be to loop over str, adding each subsequent character to the beginning of reersed so the first becomes the last and last becomes the first
+
 
 for (let i = 0; i < str.length; i++) {
     reversed = str[i] + reversed;
