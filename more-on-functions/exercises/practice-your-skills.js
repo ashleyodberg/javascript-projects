1fcdde7227d958abae0d900ad907c91e82f7f35f
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-30 14:39:36 -0800

    Worked through the first part of exercises for class 7, need more practice with recursion and mapping to solidify mapping and work out difficulties with recursion

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -1,5 +1,13 @@
 //Create an anonymous function and set it equal to a variable.
-
+let practice = function (myArg) { //define an anonymous function named practice that will check the type of the argument myArg using typeof
+    if (typeof myArg === "number") { //if 
+        return myArg * 3;
+    } else if (typeof myArg === "string") {
+        return "ARRR!";
+    } else {
+        return myArg;
+    }
+}
 /* Your function should:
 a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
@@ -13,3 +21,6 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+
+let transformedArray = ['Elocution', 21, 'Clean teeth', 100].map(practice);
+console.log(transformedArray);
