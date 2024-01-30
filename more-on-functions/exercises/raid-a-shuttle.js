1b64190dfe2ba967b5abce933ea83888cf237d1b
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-01-30 15:26:23 -0800

    worked through part 1 of this portion of the exercises

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -24,19 +24,45 @@ let cargoHold = ['meal kits', 'space suits', 'first-aid kit', 'satellite', 'gold
 console.log("Fuel level: " + checkFuel(fuelLevel));
 console.log("Hold status: " + holdStatus(cargoHold));
 
-/* Steal some fuel from the shuttle:
- * /
+// Steal some fuel from the shuttle:
  
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
+/*
+//testing the above function
+let stolenFuel = nonSuspiciousFunction(fuelLevel);
+console.log("Stolen fuel level: " + stolenFuel);*/
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
+let nonSuspiciousFunction = function (fuelLevel) {
+  let newFuelLevel = gatherNewFuel(fuelLevel);
+
+  let currentColor = checkFuel(fuelLevel);
+
+  if (currentColor === 'green') {
+    return fuelLevel - Math.min(100001, fuelLevel);
+  } else if (currentColor === 'yellow') {
+    return fuelLevel - Math.min(50001, fuelLevel);
+  } else {
+    return fuelLevel;
+  }
+};
+
+let gatherNewFuel = function (currentFuelLevel) {
+  return currentFuelLevel - 10000;
+};
+
+let stolenFuel = nonSuspiciousFunction(fuelLevel);
+console.log("Stolen fuel level: " + stolenFuel);
+
+console.log("Fuel level: " + checkFuel(nonSuspiciousFunction(fuelLevel)));
+console.log("Hold status: " + holdStatus(cargoHold));
 
 //c). Once you figure out how much fuel to pump out, return that value.
 
 //d). Decide where to best place your function call to gather our new fuel.
 
-/* Next, liberate some of that glorious cargo.
- * /
+// Next, liberate some of that glorious cargo.
+ 
 
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
@@ -46,8 +72,8 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 //d). Don’t get hasty, matey! Remember to test your function.
 
-/* Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
- * /
+// Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
+ 
  
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
 	
