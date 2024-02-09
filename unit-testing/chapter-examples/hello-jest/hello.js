2ae89c6e32c1ce8088f0b71a182682e0072d0d97
ashleyodberg <115903271+ashleyodberg@users.noreply.github.com>
2024-02-09 01:40:02 -0800

    after much troubleshooting, I was able to figure out my issues with running the code and getting the test suite to execute and have my tests pass

--- unit-testing/chapter-examples/hello-jest/hello.js
+++ unit-testing/chapter-examples/hello-jest/hello.js
@@ -1,8 +1,15 @@
-function hello(name) {
+/*function hello(name) { //takes a single argument representing person's name
    if (name === undefined)
        name = "World";
 
-   return "Hello, " + name + "!";
+   return "Hello, " + name + "!"; //and returns a string greeting that person
+} //if the hello function is called w/o an argument, it will return "Hello, World!"
+
+module.exports = hello; //to use this function outside of hello.js, we export the function*/
+
+function hello(name) {
+  if (name === undefined) name = "World";
+  return "Hello, " + name + "!";
 }
 
 module.exports = hello;
--- unit-testing/chapter-examples/hello-jest/tests/hello.test.js
+++ unit-testing/chapter-examples/hello-jest/tests/hello.test.js
@@ -1,13 +1,25 @@
-const hello = require('../hello.js');
+/*const hello = require('../hello.js');
 
-describe("hello world test", function(){
+describe("hello", function(){
 
-      test("should return a custom message when name is specified", function(){
-         expect(hello("Jest")).toBe("Hello, Jest!");
-      });
+    test("should return custom message when name is specified", function() {
+   expect(hello("Jest")).toBe("Hello, Jest!");
+});
 
-      it("should return a general greeting when name is not specified", function(){
+    test("should return a general greeting when name is not specified", function(){
         expect(hello()).toBe("Hello, World!");
     });
 
+});*/
+
+const hello = require('../hello.js');
+
+describe("hello", function(){
+  test("should return custom message when name is specified", function() {
+    expect(hello("Jest")).toBe("Hello, Jest!");
+  });
+
+  test("should return a general greeting when name is not specified", function(){
+    expect(hello()).toBe("Hello, World!");
+  });
 });
